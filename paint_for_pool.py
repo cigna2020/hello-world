ee0d74a858344cf06cf00c53c44eeb94a34e709b
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-20 18:44:55 +0300

    added the formula of calculation

--- paint_for_pool.py
+++ paint_for_pool.py
@@ -1,8 +1,18 @@
 a = float(input()) # сторона басейна, м.
 b = float(input()) # расход краски, мл.
-v = int(input()) # обьем банки, л.
+v = float(input()) # обьем банки, л.
 
-area = a**2 * 5
-consumption = (area*b)/1000
-result = consumption/v
-print(int(result))
+if a < 0 or b < 0 or v < 0:
+    print('error')
+# if type(int(a)) != int or type(int(a)) != float:
+#     print('error')
+# if type(int(b)) != int or type(int(b)) != float:
+#     print('error')
+# if type(v) != int or type(v) != float:
+#     print('error')
+else:
+    area = a**2 * 5
+    consumption = round(((area*b)/1000), 0)
+    result = consumption/v
+    print(int(result))
+
