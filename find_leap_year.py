cd009e99af94af410052ed687f2363dbcae7403d
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-20 18:07:33 +0300

    work all variants

--- find_leap_year.py
+++ find_leap_year.py
@@ -1,3 +1,3 @@
 year = int(input())
 
-print('error' if year < 1582 else 366 if year % 100 and year % 4 ==0 or year % 400 == 0 else 365)
+print('error' if year < 1582 else 366 if year % 100 and year % 4 == 0 or year % 400 == 0 else 365)
--- print_ruble.py
+++ print_ruble.py
@@ -4,11 +4,11 @@ if ruble > 99 or ruble <= 0:
     print('ошибка')
 elif str(ruble)[-1] == '1' and ruble != 11:
     print(ruble, 'рубль')
-elif str(ruble)[-1] == '2' and ruble != '12':
+elif str(ruble)[-1] == '2' and ruble != 12:
     print(ruble, 'рубля')
-elif str(ruble)[-1] == '3' and ruble != '13':
+elif str(ruble)[-1] == '3' and ruble != 13:
     print(ruble, 'рубля')
-elif str(ruble)[-1] == '4' and ruble != '14':
+elif str(ruble)[-1] == '4' and ruble != 14:
     print(ruble, 'рубля')
 else:
     print(ruble, 'рублей')
