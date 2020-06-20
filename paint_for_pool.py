9a6f298a0e210bebd4842df6b5dcae6a8accbffa
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-20 19:03:16 +0300

    added math.ceil

--- paint_for_pool.py
+++ paint_for_pool.py
@@ -1,18 +1,15 @@
+import math
+
 a = float(input()) # сторона басейна, м.
 b = float(input()) # расход краски, мл.
 v = float(input()) # обьем банки, л.
 
-if a < 0 or b < 0 or v < 0:
+if a <= 0 or b <= 0 or v <= 0:
     print('error')
-# if type(int(a)) != int or type(int(a)) != float:
-#     print('error')
-# if type(int(b)) != int or type(int(b)) != float:
-#     print('error')
-# if type(v) != int or type(v) != float:
-#     print('error')
 else:
     area = a**2 * 5
-    consumption = round(((area*b)/1000), 0)
-    result = consumption/v
-    print(int(result))
+    consumption = (area*b)/1000
+    result = math.ceil(consumption/v)
+    print(result)
+
 
