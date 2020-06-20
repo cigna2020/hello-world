d6c7dd711f87b70192d68cd73c6292b59c83721e
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-20 13:05:03 +0300

    imported necessary functions from module math in order to make the code shorter

--- find_z.py
+++ find_z.py
@@ -1,10 +1,10 @@
-import math
+from math import sqrt, pi, asin, cos
 
 x = float(input())
 y = float(input())
 
-a = (x + (math.sqrt(3)/2)*math.pi)
-b = math.sqrt((2-(math.cos(y)**2)))
+a = (x + (sqrt(3)/2)*pi)
+b = sqrt((2-(cos(y)**2)))
+z = ((asin(cos(a))) + (1.2*b))/(x**2 + y**2 + 1)
 
-z = ((math.asin(math.cos(a))) + (1.2*b))/(x**2 + y**2 + 1)
 print(round(z, 5))
