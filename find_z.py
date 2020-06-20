67a635f626c97c90b30a1c91cd2c70bf2be53003
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-20 12:57:37 +0300

    added a formula of Z

--- find_z.py
+++ find_z.py
@@ -6,4 +6,5 @@ y = float(input())
 a = (x + (math.sqrt(3)/2)*math.pi)
 b = math.sqrt((2-(math.cos(y)**2)))
 
-print(a, b)
+z = ((math.asin(math.cos(a))) + (1.2*b))/(x**2 + y**2 + 1)
+print(round(z, 5))
