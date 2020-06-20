24e8af30338bdb49165733a10f1a98966950c8df
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-20 17:14:53 +0300

    version in one line

--- find_leap_year.py
+++ find_leap_year.py
@@ -1,11 +1,3 @@
 year = int(input())
 
-if len(str(year)) < 4 or year <= 1582:
-    print('error')
-elif year % 4 == 0 and str(year)[-2:] != '00':
-    print('366')
-elif year % 4 == 0 and str(year)[-2:] == '00' and year % 100 == 0 and year % 400 == 0:
-    print('366')
-else:
-    print('365')
-
+print('error' if year < 1582 else 366 if year % 100 and year % 4 ==0 or year % 400 == 0 else 365)
