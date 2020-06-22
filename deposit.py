eb8cf35be42577acd2beed0b8cd2a3d0e1cc4c9e
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-22 12:46:40 +0300

    added formula of the calculation of profit

--- deposit.py
+++ deposit.py
@@ -1,17 +1,13 @@
 # deposit - сумма вклада, interest_rate -процентная ставка,
 # amount_months - количество месяцев
 def compute_income(deposit, interest_rate, amount_months):
-    # вычислить прибыль
-
+    # deposit, interest_rate, amount_months = x, k, n
+    s = deposit * ((1 + interest_rate / (12 * 100))**amount_months)
+    profit = s - deposit
+    print(int(profit))
 
 x = float(input())
-
 k = float(input())
-
 n = int(input())
 
-# вычислить прибыль с помощью функции
-
-s =
-
-#вывести результат
+result = compute_income(x, k, n)
