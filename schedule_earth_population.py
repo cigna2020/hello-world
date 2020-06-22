e2a48c5ebce5e7ab19447b21417de6a79d05d5e4
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-22 21:37:34 +0300

    getting line

--- schedule_earth_population.py
+++ schedule_earth_population.py
@@ -5,11 +5,28 @@ def compute_population(t):
    n = (172/45) * arcctan
    return n
 
-line = input()
-years_str_list = line.split()
+a = 2300
+b = 2400
+
+h = range(a, b, 1)
+
+years_str_list = list(h)
 years_list = list(int(x) for x in years_str_list)
 
 for z in years_list:
     print(("%5d - %6.3f миллиард(ов)" % (z, compute_population(z))))
 
-plt.gca().spines['left'].set_position('zero')
+plt.xlabel('год')
+plt.ylabel('численность населения')
+
+
+plt.gca().spines["left"].set_position('center')
+plt.gca().spines["bottom"].set_position('center')
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+
+plt.plot(years_str_list, years_list)
+
+
+
+plt.show()
