7c71dcfee320aedbdf8fdc964379847a61bc4c0c
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-22 12:35:44 +0300

    initial commit

---
+++ deposit.py
@@ -0,0 +1,17 @@
+# deposit - сумма вклада, interest_rate -процентная ставка,
+# amount_months - количество месяцев
+def compute_income(deposit, interest_rate, amount_months):
+    # вычислить прибыль
+
+
+x = float(input())
+
+k = float(input())
+
+n = int(input())
+
+# вычислить прибыль с помощью функции
+
+s =
+
+#вывести результат
