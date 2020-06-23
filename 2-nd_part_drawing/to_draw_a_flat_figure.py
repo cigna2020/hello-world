3dcadf4ee6c520949cda93adf2c75b4db5d85f26
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-23 19:26:59 +0300

    draws sector and arc

--- 2-nd_part_drawing/to_draw_a_flat_figure.py
+++ 2-nd_part_drawing/to_draw_a_flat_figure.py
@@ -7,4 +7,13 @@ y = 5
 plt.xlim(0, x)
 plt.ylim(0, y)
 
+ax = plt.gca()
+
+figure_w = Wedge((3, 1), 2, 45, 135) # рисуем сектор (центр, радиус, углы)
+ax.add_patch(figure_w)
+
+figure_a = Arc((3, 1), 6, 6, 135, 270, lw=3)
+ax.add_patch(figure_a)
+
+plt.grid()
 plt.show()
