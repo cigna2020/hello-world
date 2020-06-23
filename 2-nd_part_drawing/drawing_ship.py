2f6ee8665f99434aa0a0ea8d53549623671c8570
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-23 17:57:10 +0300

    works and draws a ship

--- 2-nd_part_drawing/drawing_ship.py
+++ 2-nd_part_drawing/drawing_ship.py
@@ -1,12 +1,10 @@
 from matplotlib.patches import Path, PathPatch
 import matplotlib.pyplot as plt
 
-n = # размер области
-
-m  = # размер области
+n = 8 # размер области
+m  = 7 # размер области
 
 plt.xlim(0, n)
-
 plt.ylim(0,m)
 
 ax = plt.gca()
@@ -15,7 +13,7 @@ ax = plt.gca()
 vertices = [(1, 3), (7, 2), (6, 1), (3, 1), (1, 3), (4, 2.5), (4,6), (7, 3), (4,2.5)]
 
 #создать список кодов для последовательности рисования:
-codes = #список
+codes = [1, 2, 2, 2, 2, 1, 2, 2, 2]
 
 
 #создать объект pyth
@@ -23,6 +21,7 @@ path = Path(vertices, codes)
 
 #создать фигуру
 path_patch = PathPatch(path, lw = 3)
+ax.add_patch(path_patch)
 
 # Добавить созданную фигуру в область ax:
 #оператор 1
