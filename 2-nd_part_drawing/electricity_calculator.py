fb41bb50cd0675edb5e09a917cf39006254d8961
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-25 18:47:57 +0300

    correct code

--- 2-nd_part_drawing/electricity_calculator.py
+++ 2-nd_part_drawing/electricity_calculator.py
@@ -1,8 +1,24 @@
-consumed = '150 132 110 186 103 229 179 224 230 113 224 202'.split()
-norm = 148
-cost_norm = 3.67
-cost_over = 4.83
+consumed = input().split()
+norm = int(input())
+cost_norm = float(input())
+cost_over = float(input())
 
 total_consumed = sum(int(consumed[i]) for i in range(len(consumed)))
+total_cost = 0
 
-print(total_consumed)
+
+for i in consumed:
+    if int(i) > norm:
+        total_cost += ((int(i) - norm) * cost_over) + (norm * cost_norm)
+    else:
+        total_cost += int(i)*cost_norm
+
+print("Сумма: %6d кВт ч, стоимость %7.2f руб" % (total_consumed, total_cost))
+
+"""
+short variant:
+m = list(map(int, input().split()))
+n, a, b = int(input()), float(input()), float(input())
+
+print("Сумма: %6d кВт ч, стоимость %7.2f руб" % (sum(m), sum(k*a if k <= n else n*a + (k-n)*b for k in m)))
+"""
