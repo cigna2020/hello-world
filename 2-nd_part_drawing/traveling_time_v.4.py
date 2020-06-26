e097d56d47d2a9eb5c91880447e1994eea42d184
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-26 14:48:32 +0300

    returns traveling time and average speed on particular parts of the road

--- 2-nd_part_drawing/traveling_time_v.4.py
+++ 2-nd_part_drawing/traveling_time_v.4.py
@@ -7,5 +7,7 @@ start = 2  #int(input())
 finish = 7 #int(input())
 
 traveling_length = np.sum(all_parts_road[start:finish+1])
+traveling_time = np.sum(all_parts_road[start:finish+1] / all_average_speed[start:finish+1])
+average_speed = traveling_length/traveling_time
 
-print(traveling_length)
+print("S = %3d км, T = %5.2f час, V = %5.2f км/ч" %(traveling_length, traveling_time, average_speed))
