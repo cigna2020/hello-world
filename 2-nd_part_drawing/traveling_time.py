256e4ddef75f97698d7e69218aabb2a0d55e980d
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-26 12:34:46 +0300

    initial commit

--- 2-nd_part_drawing/traveling_time.py
+++ 2-nd_part_drawing/traveling_time.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 
-# a = array([1, 2, 3], dtype = int)
+path = np.array([15, 5, 12, 2, 21, 17, 21, 3, 10, 5])
+speed = np.array([60, 30, 60,45, 50, 60, 50, 40, 60, 40])
 
-#
-# a = np.array([1, '2', 3], dtype = int)
-#
-# a = np.array(1, 2, 3)
-a = np.array([1, 2, 3], dtype = int)
-# print(a)
-print("a = ", a)
+len_path = path.sum() #Вычислим длину пути, можно й так: len_path = np.sum(path)
+print("Расстояние между пунктами А и В :", len_path)
 
+time = path / speed # Вычислим время прохождения автомобилем каждого участка
+print("Время на каждом участке :", np.round(time, 2))
+
+sum_time = time.sum() #  Вычислим общее время в пути
+print("Общее время в пути : ", round(sum_time, 2))
+
+avg_speed = len_path / sum_time  # Посчитаем среднюю скорость
+print("Средняя скорость : ", round(avg_speed, 2))
---
+++ 2-nd_part_drawing/traveling_time_v.2.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+road_length = input()
+road_time = input()
+average_speed = input()
