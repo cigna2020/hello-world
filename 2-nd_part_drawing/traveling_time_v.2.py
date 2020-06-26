b8cbaf2089ac599d60354aab7114e3f452cc4c87
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-26 13:05:11 +0300

    works and returns total length, total time and average speed

--- 2-nd_part_drawing/traveling_time_v.2.py
+++ 2-nd_part_drawing/traveling_time_v.2.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 
-road_length = input()
-road_time = input()
-average_speed = input()
+road_length = input().split()
+road_speed = input().split()
+
+road_length = np.array(road_length, dtype=int)
+road_speed = np.array(road_speed, dtype=int)
+
+total_length = np.sum(road_length)
+total_time = np.sum(road_length / road_speed)
+average_speed = total_length/total_time
+
+print("S = %3d км, T = %5.2f час, V = %5.2f км/ч" %(total_length, total_time, average_speed))
