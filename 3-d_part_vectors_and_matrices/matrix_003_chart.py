db178d4b8f02e68c2a161b358b2b1aab2623c95a
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-27 16:18:26 +0300

    returns average value x and y

--- 3-d_part_vectors_and_matrices/matrix_003_chart.py
+++ 3-d_part_vectors_and_matrices/matrix_003_chart.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 #задаем координаты точек
 points = np.array([[1,3],[3,-2], [2,3],[-3,4],[-2,-1]])
-
+print(points)
 # вычисляем координаты средней точки
 mean_point = np.mean(points, 0)
 print("Средняя точка: ",np.round(mean_point, 2))
--- 3-d_part_vectors_and_matrices/matrix_triangle.py
+++ 3-d_part_vectors_and_matrices/matrix_triangle.py
@@ -1,8 +1,42 @@
 import numpy as np
+import matplotlib as mt
 
 points_count = 3 #int(input())
 point_1 = np.array([-7, -8]) #np.array(input().split(), dtype=float)
 point_2 = np.array([-11, 4]) #np.array(input().split(), dtype=float)
-point_3 = np.array([-9, -5]) #np.array(input().split(), dtype=float)
-angle = 45 #int(input())
+point_3 = np.array([-9, 5]) #np.array(input().split(), dtype=float)
+angle = 5 #int(input())
+angle = np.radians(angle)
 
+massive = np.array([point_1, point_2, point_3])
+
+rotate = np.array([[np.cos(angle), np.sin(angle)], [np.sin(angle)*-1, np.cos(angle)]])
+
+multiply = np.dot(massive, rotate)
+
+average_1 = np.average(multiply[:, 0])
+average_2 = np.average(multiply[:, 1])
+
+print("avg_x = %6.2f, avg_y=%6.2f" %(average_1, average_2))
+
+
+# import numpy as np
+#
+#
+# points_count = (input())
+# point_1 = np.array(input().split(), dtype=float)
+# point_2 = np.array(input().split(), dtype=float)
+# point_3 = np.array(input().split(), dtype=float)
+# angle = np.array(input().split(), dtype=int)
+# angle = np.radians(angle[0])
+#
+# massive = np.array([point_1, point_2, point_3])
+#
+# rotate = np.array([[np.cos(angle), np.sin(angle)], [np.sin(angle)*-1, np.cos(angle)]])
+#
+# multiply = np.dot(massive, rotate)
+#
+# average_1 = np.average(multiply[:, 0])
+# average_2 = np.average(multiply[:, 1])
+#
+# print("avg_x = %6.2f, avg_y=%6.2f" %(average_1, average_2))
