df4342e18cbb9938292329ddbc18bc5089814763
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-06-27 13:33:57 +0300

    initial commit

---
+++ 3-d_part_vectors_and_matrices/matrix_triangle.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+points_count = 3 #int(input())
+point_1 = np.array([-7, -8]) #np.array(input().split(), dtype=float)
+point_2 = np.array([-11, 4]) #np.array(input().split(), dtype=float)
+point_3 = np.array([-9, -5]) #np.array(input().split(), dtype=float)
+angle = 45 #int(input())
+
